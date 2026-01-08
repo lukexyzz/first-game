0174ccb6e7b2574d3c2c3df7c6f2b5b009cab9db
lukexyzz <lukecarter382@gmail.com>
2026-01-08 15:22:33 +0000

    added circleshape and player classes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 
 
@@ -15,6 +16,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0 
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -24,6 +26,7 @@ def main():
 
         
         pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
