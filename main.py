2eee738f0084270e37f68231556afac149c24868
lukexyzz <lukecarter382@gmail.com>
2026-01-08 14:48:51 +0000

    Added FPS limit and detla time variable

--- main.py
+++ main.py
@@ -11,13 +11,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+
+    clock = pygame.time.Clock()
+    dt = 0 
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 
